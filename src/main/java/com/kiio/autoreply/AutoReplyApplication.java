67167b099f7fe681ceaa9e836ed1080a0c7b5b41
KiioYoung <330047319@qq.com>
2023-05-12 16:02:23 +0800

    Initial commit

---
+++ src/main/java/com/kiio/autoreply/AutoReplyApplication.java
@@ -0,0 +1,17 @@
+package com.kiio.autoreply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kiio
+ */
+@SpringBootApplication
+@Slf4j
+public class AutoReplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoReplyApplication.class, args);
+    }
+}
